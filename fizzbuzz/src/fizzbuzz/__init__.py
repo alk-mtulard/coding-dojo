93fe2e7c51bca81b1907cc54e8679056ad8215f1
Antoine Cezar (acezar) <92112545+alk-acezar@users.noreply.github.com>
2021-12-17 10:46:22 +0100

    Result

--- fizzbuzz/src/fizzbuzz/__init__.py
+++ fizzbuzz/src/fizzbuzz/__init__.py
@@ -1,1 +1,25 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+import sys
+
+
+def get_result(input: int) -> str:
+    is_multiple_of_3 = input % 3 == 0
+    is_multiple_of_5 = input % 5 == 0
+
+    if is_multiple_of_3 and is_multiple_of_5:
+        return "FizzBuzz"
+
+    if is_multiple_of_3:
+        return "Fizz"
+
+    if is_multiple_of_5:
+        return "Buzz"
+
+    return str(input)
+
+
+def print_fizzbuzz_for_1_to_100(output=sys.stdout):
+    for i in range(1, 101):
+        line = get_result(i) + "\n"
+        output.write(line)
--- fizzbuzz/tests/test_fizzbuzz.py
+++ fizzbuzz/tests/test_fizzbuzz.py
@@ -1,5 +1,41 @@
-from fizzbuzz import __version__
+import sys
+from io import StringIO
+
+from fizzbuzz import __version__, get_result, print_fizzbuzz_for_1_to_100
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_when_multiple_of_3_return_fizz():
+    result = get_result(3)
+
+    assert result == "Fizz"
+
+
+def test_when_multiple_of_5_return_buzz():
+    result = get_result(5)
+
+    assert result == "Buzz"
+
+
+def test_when_not_multiple_3_or_5_return_number():
+    result = get_result(1)
+
+    assert result == "1"
+
+
+def test_when_multiple_of_3_and_5_return_fizzbuzz():
+    result = get_result(15)
+
+    assert result == "FizzBuzz"
+
+
+def test_print_1_to_100():
+    output = StringIO()
+
+    print_fizzbuzz_for_1_to_100(output)
+
+    expected = "\n".join([get_result(i) for i in range(1, 101)]) + "\n"
+    assert output.getvalue() == expected
