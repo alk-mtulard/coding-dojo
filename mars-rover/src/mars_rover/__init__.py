78148798543daa1cf9cb230ae3f6997355555dcb
Antoine Cezar (acezar) <92112545+alk-acezar@users.noreply.github.com>
2022-01-07 11:05:35 +0100

    wip

--- mars-rover/src/mars_rover/__init__.py
+++ mars-rover/src/mars_rover/__init__.py
@@ -1,1 +1,51 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+
+from typing import List, NamedTuple
+
+
+class Position(NamedTuple):
+    x: int
+    y: int
+
+
+class Rover:
+    def __init__(self, position: Position, direction: str) -> None:
+        self._x = position[0]
+        self._y = position[1]
+        self._direction = direction
+
+    @property
+    def position(self) -> Position:
+        return Position(self._x, self._y)
+
+    @property
+    def direction(self) -> str:
+        return self._direction
+
+    def move(self, commands: List[str]) -> None:
+        for command in commands:
+            if command == "f":
+                self._forward()
+            if command == "b":
+                self._backward()
+            if command == "l":
+                self._direction = "W"
+            if command == "r":
+                self._direction = "E"
+
+    def _forward(self):
+        if self.direction == "N":
+            self._x += 1
+        if self.direction == "W":
+            self._y -= 1
+        if self.direction == "S":
+            self._x -= 1
+        if self.direction == "E":
+            self._y += 1
+
+    def _backward(self):
+        if self.direction == "N":
+            self._x -= 1
+        if self.direction == "S":
+            self._x += 1
--- mars-rover/tests/test_mars_rover.py
+++ mars-rover/tests/test_mars_rover.py
@@ -1,5 +1,69 @@
-from mars_rover import __version__
+from mars_rover import Position, Rover, __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_forward_increments_x_position_when_facing_north():
+    rover = Rover(Position(x=1, y=1), direction="N")
+
+    rover.move(["f"])
+
+    assert rover.position == Position(x=2, y=1)
+
+
+def test_forward_decrements_y_position_when_facing_west():
+    rover = Rover(Position(x=2, y=2), direction="W")
+
+    rover.move(["f"])
+
+    assert rover.position == Position(x=2, y=1)
+
+
+def test_forward_decrements_x_position_when_facing_south():
+    rover = Rover(Position(x=2, y=2), direction="S")
+
+    rover.move(["f"])
+
+    assert rover.position == Position(x=1, y=2)
+
+
+def test_forward_increments_y_position_when_facing_est():
+    rover = Rover(Position(x=2, y=2), direction="E")
+
+    rover.move(["f"])
+
+    assert rover.position == Position(x=2, y=3)
+
+
+def test_backward_decrements_x_when_facing_north():
+    rover = Rover(Position(x=2, y=2), direction="N")
+
+    rover.move(["b"])
+
+    assert rover.position == Position(x=1, y=2)
+
+
+def test_backward_increments_x_when_facing_south():
+    rover = Rover(Position(x=1, y=1), direction="S")
+
+    rover.move(["b"])
+
+    assert rover.position == Position(x=2, y=1)
+
+
+def test_turn_left_changes_direction():
+    rover = Rover(Position(x=1, y=1), direction="N")
+
+    rover.move(["l"])
+
+    assert rover.direction == "W"
+
+
+def test_turn_right_changes_direction():
+    rover = Rover(Position(x=1, y=1), direction="N")
+
+    rover.move(["r"])
+
+    assert rover.direction == "E"
