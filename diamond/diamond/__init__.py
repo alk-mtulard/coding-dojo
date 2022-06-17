ea21b8b2687846da28fd2bcbf205503ed8e11218
Martin Tulard (mtulard) <mtulard@salsify.com>
2022-06-17 16:38:17 +0200

    result

--- diamond/diamond/__init__.py
+++ diamond/diamond/__init__.py
@@ -1,1 +1,41 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+
+import string
+
+
+def get_diamond_border(input_letter):
+    border = ""
+
+    for letter in string.ascii_uppercase:
+        border += letter
+        if letter == input_letter:
+            break
+
+    return border
+
+
+def get_diamond(input_letter):
+    diamond = ""
+
+    border = get_diamond_border(input_letter)
+    nb_spaces = len(border) - 1
+
+    lines = []
+
+    for index, letter in enumerate(border):
+        spaces_before = " " * (nb_spaces - index)
+
+        if index == 0:
+            line = spaces_before + letter
+        else:
+            spaces_between = " " * (2 * index - 1)
+            line = spaces_before + letter + spaces_between + letter
+
+        lines.append(line)
+
+    lines.extend(lines[-2::-1])
+
+    diamond = "\n".join(lines)
+
+    return diamond
--- diamond/tests/test_diamond.py
+++ diamond/tests/test_diamond.py
@@ -1,5 +1,32 @@
 from diamond import __version__
+from diamond import get_diamond_border
+from diamond import get_diamond
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_given_a_letter_the_result_is_a_diamond_border():
+    letter = "C"
+    result = get_diamond_border(letter)
+
+    assert result == "ABC"
+
+
+def test_given_a_letter_the_result_is_a_diamond():
+    letter = "D"
+
+    result = get_diamond(letter)
+
+    assert result == "\n".join(
+        [
+            "   A",
+            "  B B",
+            " C   C",
+            "D     D",
+            " C   C",
+            "  B B",
+            "   A",
+        ]
+    )
